7d609806c6fe4e7169ebe94f3ac026fcfd2dd2ad
Joezari Borlongan <bor.joezari@gmail.com>
2026-01-16 11:48:21 -0800

    chore(validation): handle errors thrown by @Valid annotations.

--- src/main/java/com/familyhub/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/familyhub/demo/exception/GlobalExceptionHandler.java
@@ -2,11 +2,19 @@ package com.familyhub.demo.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 
 @Slf4j
@@ -32,7 +40,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
     }
 
+    @Override
+    protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<ErrorResponse.ValidationError> errorList = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> new ErrorResponse.ValidationError(error.getField(), error.getDefaultMessage()))
+                .toList();
 
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                path,
+                "Bad Request",
+                errorList
+        );
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, HttpServletRequest request, String message) {
         ErrorResponse error = new ErrorResponse(status.value(), request.getRequestURI(), message);
