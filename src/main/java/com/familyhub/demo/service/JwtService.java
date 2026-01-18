bccc1248d48f89d5d528952d8fc2aa72165e1662
Joezari Borlongan <bor.joezari@gmail.com>
2026-01-18 05:47:00 -0800

    feat: add service responsible for generating and validating JWTs

---
+++ src/main/java/com/familyhub/demo/service/JwtService.java
@@ -0,0 +1,59 @@
+package com.familyhub.demo.service;
+
+import com.familyhub.demo.config.JwtConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    private final JwtConfig jwtConfig;
+
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuer(jwtConfig.getIssuer())
+                .signWith(getSignInKey())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtConfig.getExpiration()))
+                .compact();
+    }
+
+    public void validateToken(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+         if(!userDetails.getUsername().equals(username)) {
+             throw new RuntimeException("JWT subject mismatch");
+         }
+    }
+
+    private String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parser()
+                .verifyWith((SecretKey) getSignInKey())
+                .requireIssuer(jwtConfig.getIssuer())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.getSecret());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
