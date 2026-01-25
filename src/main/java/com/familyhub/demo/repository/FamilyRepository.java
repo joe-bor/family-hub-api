8947f47478569910c07c53deab8e73411ea1ecbb
Joezari Borlongan <bor.joezari@gmail.com>
2026-01-25 12:31:47 -0800

    feat: made repository layer return optional; to be handled by service layer

--- src/main/java/com/familyhub/demo/repository/FamilyRepository.java
+++ src/main/java/com/familyhub/demo/repository/FamilyRepository.java
@@ -3,9 +3,10 @@ package com.familyhub.demo.repository;
 import com.familyhub.demo.model.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FamilyRepository extends JpaRepository<Family, UUID> {
     boolean existsByUsername(String username);
-    Family findByUsername(String username);
+    Optional<Family> findByUsername(String username);
 }
