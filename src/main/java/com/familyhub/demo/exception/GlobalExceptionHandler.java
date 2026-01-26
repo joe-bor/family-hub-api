a1fc7cd260ecc7cdcf1b83553b5a22e2fcc22f38
Joezari Borlongan <bor.joezari@gmail.com>
2026-01-26 15:30:47 -0800

    feat: add new exception handler for `InvalidCredentialException`

--- src/main/java/com/familyhub/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/familyhub/demo/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,6 +35,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorResponse> handleUsernameAlreadyExist(UsernameAlreadyExists ex, HttpServletRequest request) {
         return buildErrorResponse(
                 HttpStatus.CONFLICT,
+                request,
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(InvalidCredentialException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredential(InvalidCredentialException ex, HttpServletRequest request) {
+        return buildErrorResponse(
+                HttpStatus.UNAUTHORIZED,
                 request,
                 ex.getMessage()
         );
